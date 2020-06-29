e23174036be1d89540b49a8c0df1b5c3e2a86405
Serg Ch <chivirdos@gmail.com>
2020-06-29 22:17:53 +0300

    Поправил отображение записей в таблице

--- src/views/LegalEntity.vue
+++ src/views/LegalEntity.vue
@@ -5,14 +5,18 @@
     <table>
       <thead>
         <tr>
-          <td id="firstColumnName">Legal Entity</td>
+          <td class=""></td>
+          <td>Legal Entity</td>
           <td>Street</td>
           <td>City</td>
           <td>Country</td>
         </tr>
       </thead>
       <tbody> 
-        <tr v-for="entity in entities" :key="entity.id">
+        <tr v-for="entity in entities" :key="entity.id" :value="entity.id" class="row" @click="onTableClickHandler">
+          <td>
+            <i class="fa fa-check" aria-hidden="true"></i>
+          </td>
           <td>{{entity.legalEntityName}}</td>
           <td>{{entity.address1 + ' ' + entity.address2}}</td>
           <td>{{entity.city}}</td>
@@ -20,7 +24,7 @@
         </tr>
       </tbody>
     </table>
-    <button class="content__button">Select Pharmacies</button>
+    <button class="content__button" @click.prevent="selectPharmacy">Select Pharmacies</button>
 </div>
 </template>
 
@@ -28,11 +32,27 @@
 export default {
   name: 'legalentity',
   data: () => ({
-    entities: null
+    entities: null,
+    rows: null
   }),
   async mounted() {
     this.entities = await this.$store.dispatch('fetchEntities')
-    console.log(this.entities)
+  },
+  methods: {
+    onTableClickHandler() {
+      const rows = document.querySelectorAll('.row')
+      for (let row of rows) {
+        row.onclick = function() {
+          for (let children of rows) {
+              children.classList.remove('active')
+          }
+          this.classList.toggle('active')
+        }
+      }
+    },
+    selectPharmacy() {
+      console.log('Button clicked!')
+    }
   }
 }
 </script>
